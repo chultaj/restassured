cfcf9c345a22cc751cffd3892062b9e32e935c15
Pawel Naryniecki <pawel.naryniecki@gmail.com>
2019-02-16 16:45:59 +0100

    Add test 2

--- src/test/java/FindByStatus.java
+++ src/test/java/FindByStatus.java
@@ -1,7 +1,15 @@
+import io.restassured.RestAssured;
+import io.restassured.mapper.TypeRef;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import io.restassured.response.ResponseBodyExtractionOptions;
+import io.restassured.response.ValidatableResponse;
 import org.junit.jupiter.api.Test;
 
-import java.math.BigDecimal;
+import java.util.List;
+import java.util.Map;
 
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.hasItems;
 
@@ -15,6 +23,15 @@ public class FindByStatus extends BaseTest {
                 when().get(findByStatus).
                 then().statusCode(200).
                 and().body("id", hasItems(9199424981609282123L));
+    }
 
+    @Test
+    public void testAllSold() {
+        Response response = given().param("status", "sold").
+                when().get(findByStatus).
+                then().statusCode(200).
+                extract().response();
+
+        List<Map<String, Object>> pets = response.as(new TypeRef<List<Map<String, Object>>>() {});
     }
 }
